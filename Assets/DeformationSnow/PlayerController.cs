e6b58eaa79e7b41b225b5c7a38e3ca36ae8e50d1
Jonas Sandstedt <git@jonassandstedt.se>
2022-01-30 02:08:17 +0100

    Juice

--- Assets/DeformationSnow/PlayerController.cs
+++ Assets/DeformationSnow/PlayerController.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Linq;
 using System.Numerics;
+using Cinemachine;
 using DeformationSnow;
 using TMPro;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
+using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
@@ -21,16 +24,23 @@ public class PlayerController : MonoBehaviour
     private Vector2 _move;
     private bool _inAir;
     private float _inAirCooldown;
+    private bool _inAirLastFrame;
     private Vector3 _previousPosition;
     private bool _jump;
     private bool _sprint;
     private float _stunnedCooldown;
+    [SerializeField] private bool _hitGroundTrigger;
+    private bool _onIsland;
 
     public ParticleSystem snowParticles;
-
+    private ParticleSystem.EmissionModule _em;
+    public GameObject groundCollisionParticles;
+    public CinemachineImpulseSource _impulseSource;
+    
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _em = snowParticles.emission;
     }
 
     public void OnMove(InputValue value)
@@ -81,9 +91,10 @@ public class PlayerController : MonoBehaviour
                 var hitGround = Physics.Raycast(transform.position, Vector3.down, 1f);
                 if (hitGround) _inAir = false;
             }
-
+            _inAirLastFrame = true;
             return;
         }
+        _hitGroundTrigger = _inAirLastFrame && !_inAir;
 
         if (_rigidbody.velocity.magnitude > 10f)
         {
@@ -104,9 +115,10 @@ public class PlayerController : MonoBehaviour
 
         var direction = new Vector3(_move.x, 0, _move.y);
 
+        bool grounded = true;
         if (_jump)
         {
-            var grounded = Physics.OverlapSphere(transform.position, transform.localScale.x * .6f).Length > 1;
+            grounded = Physics.OverlapSphere(transform.position, transform.localScale.x * .6f).Length > 1;
 
             if (grounded)
             {
@@ -148,18 +160,36 @@ public class PlayerController : MonoBehaviour
         _previousPosition = transform.position;
 
         _jump = false;
+        
+        if (_inAir || _onIsland)
+        {
+            _em.enabled = false;
+            _em.rateOverTime = 0f;
+        }
+        else
+        {
+            _em.enabled = true;
+            // < xf ? - dont emit any particle below this value
+            // * xf - Emitter multiplier
+            _em.rateOverTime = (_rigidbody.velocity.magnitude < 10f ? 0f : _rigidbody.velocity.magnitude * 30f);
+        }
 
-        var em = snowParticles.emission;
-        em.enabled = true;
-        // < xf ? - dont emit any particle below this value
-        // * xf - Emitter multiplier
-        em.rateOverTime = (_rigidbody.velocity.magnitude < 10f ? 0f : _rigidbody.velocity.magnitude * 30f);
+        if (_hitGroundTrigger)
+        {
+            var go = Instantiate(groundCollisionParticles, this.transform.position, Quaternion.identity);
+            Destroy(go, 5f); // TODO: make a safer destory
+            _impulseSource.GenerateImpulse();
+            
+        }
+        _inAirLastFrame = false; // using the early return here
     }
 
     private void AddExtraGravityIfOnIsland()
     {
+        _onIsland = false;
         if (Physics.OverlapSphere(transform.position, 2f).Any(hit => hit.CompareTag("Island")))
         {
+            _onIsland = true;
             _rigidbody.AddForce(Vector3.down * 200f * Time.deltaTime, ForceMode.Acceleration);
         }
     }
