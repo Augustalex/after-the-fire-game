36bb5a89e115aea7fecc5c3a26c5225bf9c33173
August Alexandersson <augustalexandersson@gmail.com>
2022-03-12 15:38:59 +0100

    Removed Unity starter assets

--- Assets/PlayerModeController.cs
+++ Assets/PlayerModeController.cs
@@ -1,11 +1,7 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using StarterAssets;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Random = UnityEngine.Random;
 
 public class PlayerModeController : MonoBehaviour
 {
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using StarterAssets;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
