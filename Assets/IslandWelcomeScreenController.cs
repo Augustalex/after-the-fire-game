1a4f97bc1667dcadaa4446bd1257958f13387d92
August Alexandersson <augustalexandersson@gmail.com>
2022-04-17 13:28:59 +0200

    WIP

---
+++ Assets/IslandWelcomeScreenController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandWelcomeScreenController : MonoBehaviour
+{
+    public GameObject welcomeScreen;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            
+        }
+    }
+}
